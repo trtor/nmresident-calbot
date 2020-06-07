7a012477e79688b9f555ded27e348abe2a71015a
trtor <49577823+trtor@users.noreply.github.com>
2020-06-07 12:45:20 +0700

    Add feature weekly reminder

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 require("dotenv").config();
 const CronJob = require("cron").CronJob;
-
 const { readCalendar } = require("./readCal");
 const { sendNotify } = require("./lineNotify");
-
-const job = new CronJob(
-  process.env.PROCESS_INTERVAL_CRON,
+/*  */
+console.log("Start Cron Timer");
+/*  */
+const jobDaily = new CronJob(
+  process.env.PROCESS_DAILY_INTERVAL_CRON,
   () => {
-    runInterval();
+    runDaily();
   },
   null,
   true,
   "Asia/Bangkok"
 );
+jobDaily.start();
 
-job.start();
-console.log("Start Cron Timer");
-
-async function runInterval() {
+async function runDaily() {
   const msg = await readCalendar({
     icalURL: process.env.ICAL_URL,
     mode: process.env.MODE, // offline | online
-    fwdHour: process.env.LOOKFORWARD_MAX_HOUR,
+    fwdHour: process.env.LOOKFORWARD_DAILY_MAX_HOUR,
   });
-  console.log("Cron job fetch calendar was triggered");
+  console.log("Cron job fetch daily calendar was triggered");
+
+  if (msg)
+    await sendNotify({
+      token: process.env.LINE_TOKEN,
+      options: {
+        message: msg,
+        notificationDisabled: false,
+      },
+    });
+}
+/* ##################################################### */
+const jobWeekly = new CronJob(
+  process.env.PROCESS_WEEKLY_INTERVAL_CRON,
+  () => {
+    runWeekly();
+  },
+  null,
+  true,
+  "Asia/Bangkok"
+);
+jobWeekly.start();
+
+async function runWeekly() {
+  const msg = await readCalendar({
+    icalURL: process.env.ICAL_URL,
+    mode: process.env.MODE, // offline | online
+    fwdHour: process.env.LOOKFORWARD_WEEKLY_MAX_HOUR,
+  });
+  console.log("Cron job fetch weekly calendar was triggered");
 
   if (msg)
     await sendNotify({
@@ -35,4 +63,4 @@ async function runInterval() {
     });
 }
 
-// runInterval();
+runWeekly();
